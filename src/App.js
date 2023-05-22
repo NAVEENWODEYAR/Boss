54e02d796e19109bba5f2c552ce87c0e46fab2ee
Naveen <naveenkrishnawodeyar@gmail.com>
2023-05-22 14:33:15 +0530

    Class Compo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
-import Comp1 from "./Components/Comp1";
-import Header from "./Components/Header";
-import Body from "./Components/Body";
-import Footer from "./Components/Footer";
-import Test1 from "./Components/Test1";
+// import Comp1 from "./Components/LearnerComp/Comp1";
+// import Header from "./Components/Header";
+// import Body from "./Components/Body";
+// import Footer from "./Components/Footer";
+// import Test1 from "./Components/TestCompo/Test1";
+import Header from "./Components/ClassCompo/Header";
 
 // function App()
 // {
@@ -30,7 +31,8 @@ function App()
         <hr/>
         <Footer/> */
         
-        <Test1/>
+        // <Test1/>
+        <Header/>
         
         }
     </div>
---
+++ src/Components/ClassCompo/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import bimage from "./Images/b.jpeg";
+import nimage from './Images/n.jpg';
+class Header extends React.Component
+{
+    // class members
+    name = "Boss";
+    age = 25;
+    details = {eID:1,Designation:"Manager"};
+    // image = "N:\Pro\FS\SPA\boss\src\Components\ClassCompo\b.jpeg";
+
+    render ()
+    {
+        return (
+            <div>
+                <h2> This is the Header component of the Header class.</h2>
+                <hr />
+                <h3> Employee-Name : {this.name}</h3>
+                <h3> Employee-Age :{this.age}</h3>
+                <h3> Employee-Details : {this.details.Designation}</h3>
+                <img src={bimage} alt="" />  
+                <hr />
+                <p> Details = {JSON.stringify(this.details)}</p>
+
+                
+                <hr />
+                <img src={nimage} alt="" />
+
+            </div>
+        );
+    }
+}
+export default Header
