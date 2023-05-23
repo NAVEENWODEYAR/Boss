6fb1a445f35bbf232a21f52b113f9c5e1bf3ec7f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-05-23 15:16:36 +0530

    ga

---
+++ src/Boss.js
@@ -0,0 +1,40 @@
+
+// import Comp1 from "./Components/LearnerComp/Comp1";
+// import Header from "./Components/Header";
+// import Body from "./Components/Body";
+// import Footer from "./Components/Footer";
+// import Test1 from "./Components/TestCompo/Test1";
+import Header from "./Components/ClassCompo/Header";
+
+// function App()
+// {
+
+
+//     return <div>
+//         <Comp1/>
+//     </div>
+// }
+
+// export default App
+
+
+function App()
+{
+
+    return <div>
+        {/* <Comp1/>
+        <hr/>
+        <hr/>
+        <Header/>
+        <hr/>
+        <Body/>
+        <hr/>
+        <Footer/> */
+        
+        // <Test1/>
+        <Header/>
+        
+        }
+    </div>
+}
+export default App
---
+++ src/Components/ClassCompo/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component 
+{
+    render()
+    {
+        return ( <div>
+                <h3> Footer section is the important thing to display various links </h3>
+        </div>);
+    }
+}
+
+export default Footer
