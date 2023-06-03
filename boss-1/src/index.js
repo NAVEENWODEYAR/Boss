bcbfda5baf763b12b047143ca59648902c5c7dc1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-04 01:17:33 +0530

    GM

--- boss-1/src/index.js
+++ boss-1/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import App from './Components/router dom/App';
 
 
-    const root = ReactDom.createRoot(document.getElementById('root'));
-    root.render(<App />)
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
+
+    <App />
+)
