6b0f68d6401454fe5effd1c61f4fff4fa1f0de08
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-11 23:52:49 +0530

    gn

--- boss-1/src/Redux/redux/store.jsx
+++ boss-1/src/Redux/redux/store.jsx
@@ -3,6 +3,6 @@ import {createStore} from 'redux';
 import { msgReducer } from './message/message.reducer';
 import {composeWithDevTools} from '@redux-devtools/extension';
 
-let store = createStore(msgReducer,composeWithDevTools);
+let store = createStore(msgReducer,composeWithDevTools());
 
 export {store};
