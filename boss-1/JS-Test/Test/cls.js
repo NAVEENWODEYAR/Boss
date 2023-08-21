caba002acb6180e4074dffefbb175e24f5ca34ee
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 16:28:17 +0530

    tt

--- boss-1/JS-Test/Test/cls.js
+++ boss-1/JS-Test/Test/cls.js
@@ -1,12 +1,14 @@
 class Test
 {
      cName = 'Boss';
+     cId;
     getMsg()
         {
             console.log(this.cName);
+            console.log(this.cId);
         }
 }
 var a = new Test();
 console.log(a);
 console.log(a.getMsg);
-console.log(a.getMsg());
+// console.log(a.getMsg());
