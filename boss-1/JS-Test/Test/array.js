9d7bc8ae1e8f3a626e2170e524c3d5afbe957f65
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 16:10:11 +0530

    ge

---
+++ boss-1/JS-Test/Test/array.js
@@ -0,0 +1,7 @@
+let arr = [1,5,7,7,4,2,4,8,9];
+
+for (var v of arr) 
+{
+    console.log(v);    
+}
+
---
+++ boss-1/JS-Test/Test/object.js
@@ -0,0 +1,11 @@
+let arr = {
+             id:1,
+             name:"Test",
+             class:8
+            };
+
+for (var v in arr) 
+{
+    console.log(v+"-"+arr[v]);    
+}
+
